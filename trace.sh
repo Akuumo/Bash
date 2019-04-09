9c19344a0bfe111b4bd0449ba186905f01deb0b0
Akuumo <mesnil.sam.pro@hotmail.com>
2019-04-09 14:52:13 +0200

    Finish First Step

--- trace.sh
+++ trace.sh
@@ -1,14 +1,54 @@
-cription:												    #
-# Ce script a pour but de tracer le chemin d'un paquet et d'en créer un cartographie.			    #
-# 													    #
-# Le script pourra tracer la route du paquet avec differents protocoles et sur différents numéros de ports. #
-#													    #
-# Outils utilisés:  traceroute, xdot, cut, grep, sed.							    #
-# 													    #
-#############################################################################################################
+#!/bin/bash
 
+ttls()
+{
+	argLst=( "-I" "-T -p 22" "-T -p 80" "-T -p 443" "-U -p 1194" "-U -p 5060" )
+	prtLst=( "ICMP" "TCP 22" "TCP 80" "TCP 443" "UDP 1194" "UDP 5060" )
 
-# Cette boucle permet de vérifier la présence d'un argument.  
+	for ttl in $(seq 1 30)  
+	do
+		getIpAddr "$ttl" argLst[@] "$ipT" prtLst[@]
+		if [ $code == 0 ]
+		then
+			return 0
+		fi	
+	done	
+}
+
+getIpAddr()
+{
+	ttl=$1
+	declare -a prots=("${!2}")	
+	ipT=$3
+	declare -a prtnm=("${!4}")	
+	#echo "traceroute -f $ttl -m $ttl -q 1 $prot $ipT -n | grep -v traceroute | cut -c 5- | cut -d \" \" -f 1" 
+
+	for prot in "${prots[@]}"  
+	do
+		hop="$(traceroute -f $ttl -m $ttl -q 1 $prot $ipT -n | grep -v traceroute | cut -c 5- | cut -d " " -f 1)"
+		
+		if [ "'$hop'" != "'*'" ]
+		then
+			echo "$ttl) Success $hop with $prot"
+			nmap -Pn -A -T4 $hop
+
+			if [ $hop == $ipT ]
+			then 
+				code=0
+				return $code
+			else
+				code=1
+				return $code
+			fi	
+		else
+			echo "$ttl) Echec with $prot"
+		fi
+		
+	done
+	echo "?"
+	return -1
+}
+
 if [ -z $1 ]						      
 then 						      
 	echo " Pas de paramètre "		      
@@ -19,68 +59,15 @@ clear
 ################################################################
 #  Permet de trouver l'adresse IP sur laquel debute le paque   #
 ################################################################
-tshark -a duration:5 -Y icmp host $1 > me.txt | ping $1 -c 3
-host="$(cat me.txt | cut -c 4-| head -n 1 | cut -d " " -f 6)"
-echo $host
+#tshark -a duration:5 -Y icmp host $1 > me.txt | ping $1 -c 3
+#host="$(cat me.txt | cut -c 4-| head -n 1 | cut -d " " -f 6)"
+#echo $host
 
-rm me.txt 
+#rm me.txt 
 ################################################################ 
-ipTrgt=$1
-fileNm="$(echo $ipTrgt | sed 's/\./\-/g').route"
 
+#fileNm="$(echo $ipTrgt | sed 's/\./\-/g').route"
+#echo -n  '"'"$host"'"' "-- " > $fileNm
 
-echo -n  '"'"$host"'"' "-- " > $fileNm
-
-ttl=1
-hop=""
-hopN1=""
-listArg=( "-I" "-T -p 22" "-T -p 80" "-T -p 443" "-U -p 1194" "-U -p 5060"  )
-
-
-for ttl in $(seq 1 30)  
-do
-	for proto in "${listArg[@]}" 
-	do
-		hop=$( traceroute -f $ttl -m $ttl -q 1 $proto $IpCible -n | grep -v traceroute | cut -c 5- | cut -d " " -f 1  )
-		hopN1=$(traceroute -A -f $(($ttl+1)) -m $(($ttl+1)) -q 1 $proto $IpCible -n |grep -v traceroute | cut -c 5- | cut -d " " -f 1,2  )
-																																							
-		if [ "$Hop" == "$IpCible" ]
-		then   
-			echo -n  '"'" $Hop "'"'"; " >> $NomFichier
-			break 2
-		elif [ "$Hop" == "*" ] && [ "$proto" == "-I" ] && [ "$HopPlusUn" == "*" ]
-		then
-			echo -n " Inconnuedesaut$ttl" >> $NomFichier
-			echo -n " -- "'"' "Inconnuedesaut$ttl" '"'" -- " >> $NomFichier
-			break
-		elif [ "$Hop" == "*" ] && [ "$proto" == "-I" ] && [ "$HopPlusUn" != "*" ]
-		then
-			echo -n " Inconnuedesaut$ttl" >> $NomFichier
-			echo -n " -- "'"' "Réseau avant $HopPlusUn & saut $ttl & adresse finale $IpCible" '"'" -- " >> $NomFichier
-			break
-		elif [ "$Hop" != "*" ] && [ "$Hop" != "$IpCible" ]
-		then
-			echo -n  '"'" $Hop "'"'  >> $NomFichier
-			echo -n " -- "'"' "Réseau avant $HopPlusUn & saut $ttl & adresse finale $IpCible" '"'" -- " >> $NomFichier
-			break
-		fi
-	done
-done
-
-																																																																																																																																# Deuxieme ligne	
-																																																																																																																																	echo "" >> $NomFichier
-																																																																																																																																		for ttl2 in $(seq 1 $ttl)
-																																																																																																																																					do	
-																																																																																																																																									if [ "$ttl2" == "$ttl" ]
-																																																																																																																																													then
-																																																																																																																																																		echo -n " $(cat $NomFichier |grep $Moi | sed 's/ -- / -/g' | cut -d "-" -f $((($ttl2*2)+1)) )" >> test
-																																																																																																																																																					else
-
-																																																																																																																																																										echo -n " , $(cat $NomFichier |grep $Moi | sed 's/ -- / -/g' |sed 's/;/ -/g' | cut -d "-" -f $((($ttl2*2)+1)) )" >> test
-																																																																																																																																																												fi
-																																																																																																																																																														done
-																																																																																																																																																															echo -n " [shape=ellipse,fontcolor=white,fixedsize=true];" >> test
-																																																																																																																																																																echo -n "$(cat test | sed -e 's/ /"'$Moi'" /')" >> $NomFichier
-																																																																																																																																																																	echo ""	>> $NomFichier
-																																																																																																																																																																		echo "$(cat $NomFichier | sed 's/'[*]'/[65535]/g')" > $NomFichier
-																																																																																																																																																																		rm test
+ipT=$1
+ttls "$ipT"
